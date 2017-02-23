e7b7f98d75fbef88ea7658e7f4556c1f202c622e
popalexandru06 <alexandrupop@reea.net>
2017-02-23 10:16:40 +0200

    Map fields and file import

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,5 @@
 class Campaign < ApplicationRecord
   has_many :products
+  
+  validates_uniqueness_of :source_id
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
-  belongs_to :campaign
-  belongs_to :brand
-  belongs_to :widget
+  belongs_to :campaign, optional: true
+  belongs_to :brand, optional: true
+  belongs_to :widget, optional: true
   has_many :images
+
+  validates_uniqueness_of :source_id
 end
